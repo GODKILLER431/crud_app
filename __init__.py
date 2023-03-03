626bab5a163ca7a9edcdbfa95f684b98d56d782c
Suthirtha Paramanick <suthirthaparamanick48@gmail.com>
2023-03-03 12:45:10 +0530

    initial commit

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from myPackage.myModule import *
---
+++ myModule.py
@@ -0,0 +1,6 @@
+def add():
+    m=int(input("Enter a="))
+    n=int(input("Enter b="))
+    print("Sum Result:",(m+n))
+def mult(x,y):
+    print("Mult Result=",(x*y))
